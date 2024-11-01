dd19174b88154937c75d0d61ca6b1aba7e2dd75e
Barbara Poblete <bpobletem96@gmail.com>
2024-11-01 12:45:25 -0300

    keys de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDSGn0CfN8bDo7UD9-IeQvHMFuMIMQIBh8",
+    authDomain: "tellevoapp-16716.firebaseapp.com",
+    projectId: "tellevoapp-16716",
+    storageBucket: "tellevoapp-16716.firebasestorage.app",
+    messagingSenderId: "237458071393",
+    appId: "1:237458071393:web:a22f819922c6fdfa466f20"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDSGn0CfN8bDo7UD9-IeQvHMFuMIMQIBh8",
+    authDomain: "tellevoapp-16716.firebaseapp.com",
+    projectId: "tellevoapp-16716",
+    storageBucket: "tellevoapp-16716.firebasestorage.app",
+    messagingSenderId: "237458071393",
+    appId: "1:237458071393:web:a22f819922c6fdfa466f20"
+  }
 };
 
 /*
