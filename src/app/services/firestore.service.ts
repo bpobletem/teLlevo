f15dcb20eb3248c4e4278a1619067c00fd3d32fb
Barbara Poblete <bpobletem96@gmail.com>
2024-11-01 14:25:05 -0300

    get colecciones fire service

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
-
+  firestore: Firestore = inject(Firestore)
   constructor() { }
+
+  getCollectionChanges<tipo>(path: string) {
+    const itemCollection = collection(this.firestore, path);
+    return collectionData(itemCollection) as Observable<tipo[]>;
+  }
 }
