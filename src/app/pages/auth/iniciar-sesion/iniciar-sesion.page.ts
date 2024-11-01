ab9a96aff7ab07ca28f3539205028e14e402f5cc
Barbara Poblete <bpobletem96@gmail.com>
2024-11-01 15:00:11 -0300

    rename firebase + login auth

--- src/app/pages/auth/iniciar-sesion/iniciar-sesion.page.ts
+++ src/app/pages/auth/iniciar-sesion/iniciar-sesion.page.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/interfaces';
 import { StorageService} from 'src/app/services/datos.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -14,14 +16,26 @@ export class IniciarSesionPage implements OnInit {
   loginForm: FormGroup;
   errorMessage: string = '';
 
+  firebaseSrv = inject(FirebaseService)
+
   constructor(private formBuilder: FormBuilder, private router: Router, private srv:StorageService,) {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      correo: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
     });
   }
 
   ngOnInit(): void {
     
+  }
+
+  submit() {
+    if (this.loginForm.valid) {
+      this.firebaseSrv.signIn(this.loginForm.value as Usuario).then(res => {
+        console.log("Usuario autenticado")
+        console.log(res)
+        this.router.navigate(['/home'])
+      })
+    }
   }
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,13 +1,23 @@
 import { inject, Injectable } from '@angular/core';
 import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Auth, getAuth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Usuario } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FirestoreService {
+export class FirebaseService {
   firestore: Firestore = inject(Firestore)
+  auth = inject(Auth)
   constructor() { }
+
+
+  /// auth
+
+  signIn(user: Usuario){
+    return signInWithEmailAndPassword(getAuth(), user.correo, user.password)
+  }
 
   getCollectionChanges<tipo>(path: string) {
     const itemCollection = collection(this.firestore, path);
