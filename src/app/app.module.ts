d96d133f616df2ace6fc90ecbf2a7f2ed91d3f5a
Barbara Poblete <bpobletem96@gmail.com>
2024-11-01 15:00:24 -0300

    cambio keys firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
@@ -20,7 +21,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, IonicStorageModule.forRoot(),],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
-    provideFirebaseApp(() => initializeApp({"projectId":"tellevoapp-16716","appId":"1:237458071393:web:a22f819922c6fdfa466f20","storageBucket":"tellevoapp-16716.firebasestorage.app","apiKey":"AIzaSyDSGn0CfN8bDo7UD9-IeQvHMFuMIMQIBh8","authDomain":"tellevoapp-16716.firebaseapp.com","messagingSenderId":"237458071393"})), 
+    provideFirebaseApp(() => initializeApp(environment.firebase)), 
     provideAuth(() => getAuth()), 
     provideFirestore(() => getFirestore())
   ],
