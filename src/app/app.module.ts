dcf6b7e5927981675b95e758eb3d737715af0d59
Barbara Poblete <bpobletem96@gmail.com>
2024-11-01 12:49:18 -0300

    integracion firebase: auth y store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, IonicStorageModule.forRoot(),],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    provideFirebaseApp(() => initializeApp({"projectId":"tellevoapp-16716","appId":"1:237458071393:web:a22f819922c6fdfa466f20","storageBucket":"tellevoapp-16716.firebasestorage.app","apiKey":"AIzaSyDSGn0CfN8bDo7UD9-IeQvHMFuMIMQIBh8","authDomain":"tellevoapp-16716.firebaseapp.com","messagingSenderId":"237458071393"})), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
