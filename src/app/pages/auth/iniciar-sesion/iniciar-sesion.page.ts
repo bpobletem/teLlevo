45cd3d7a9aac4dcc20a2b40b87bc34b202dcddd0
Barbara Poblete <bpobletem96@gmail.com>
2024-11-05 10:00:38 -0300

    iniciar sesion

--- src/app/pages/auth/iniciar-sesion/iniciar-sesion.page.ts
+++ src/app/pages/auth/iniciar-sesion/iniciar-sesion.page.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { StorageService } from './../../../services/storage.service';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/interfaces/interfaces';
-import { StorageService} from 'src/app/services/datos.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
@@ -14,14 +14,18 @@ import { UtilsService } from 'src/app/services/utils.service';
 })
 export class IniciarSesionPage implements OnInit {
 
-  loginForm: FormGroup;
+  correo = '';
+  password = '';
+
+  form: FormGroup;
   errorMessage: string = '';
 
   firebaseSrv = inject(FirebaseService);
   utilsSrv = inject(UtilsService);
+  localStorageSrv = inject(StorageService)
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private srv:StorageService,) {
-    this.loginForm = this.formBuilder.group({
+  constructor(private formBuilder: FormBuilder, private router: Router, private srv: StorageService,) {
+    this.form = this.formBuilder.group({
       correo: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
     });
@@ -32,12 +36,12 @@ export class IniciarSesionPage implements OnInit {
   }
 
   async submit() {
-    if (this.loginForm.valid) {
+    if (this.form.valid) {
       const loading = await this.utilsSrv.loading();
       await loading.present();
-      this.firebaseSrv.signIn(this.loginForm.value as Usuario).then(res => {
-        console.log("Usuario autenticado");
-        console.log(res);
+
+      this.firebaseSrv.signIn(this.form.value as Usuario).then(res => {
+        this.getUser(res.user.uid);
         this.router.navigate(['/home']);
       }).catch(error => {
         this.utilsSrv.presentToast({
@@ -51,6 +55,40 @@ export class IniciarSesionPage implements OnInit {
       }).finally(() => {
         loading.dismiss();
       })
+    }
+  }
+
+  async getUser(uid: string) {
+    if (this.form.valid) {
+      const loading = await this.utilsSrv.loading();
+      await loading.present();
+
+      let path = `Usuario/${uid}`;
+
+      this.firebaseSrv.getDocument(path).then( (user: Usuario) => {
+          this.localStorageSrv.set(this.correo, user);
+          this.router.navigate(['/home']);
+          this.form.reset();
+
+          this.utilsSrv.presentToast({
+            message: `Bienvenido, ${user.nombre}`,
+            duration: 1500,
+            color: 'primary',
+            position: 'bottom',
+            icon: 'person-circle-outline'
+          });
+        }).catch(error => {
+          this.utilsSrv.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'primary',
+            position: 'bottom',
+            icon: 'alert-circle-outline'
+          });
+          console.log(error);
+        }).finally(() => {
+          loading.dismiss();
+        });
     }
   }
 }
