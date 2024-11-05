55b01e70781f68e181b51598bf0a3b797f010fa1
Barbara Poblete <bpobletem96@gmail.com>
2024-11-05 10:01:42 -0300

    reset homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,33 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
-  isLoggedIn = false;
+  isLoggedIn = true;
 
   constructor(private router: Router) {}
 
-  ionViewWillEnter() {
-    this.isLoggedIn = localStorage.getItem('userLoggedIn') === 'true';
-  }
-
-  logout() {
-    localStorage.removeItem('userLoggedIn');
-    this.isLoggedIn = false;
-    this.router.navigate(['/iniciar-sesion']);
-  }
-  
-  //Hacer uso de los datos guardados con localStorage da problemas con ngOnInit. El método de arriba funciona, así que se usará por ahora.
-  //   isLoggedIn = false;
-
-//   constructor(private router: Router) {}
-
-//   ngOnInit() {
-//     this.isLoggedIn = localStorage.getItem('userLoggedIn') === 'true';
-//   }
-
-//   logout() {
-//     localStorage.removeItem('userLoggedIn');
-//     this.isLoggedIn = false;
-//     this.router.navigate(['/iniciar-sesion']); 
-//   }
 }
 
