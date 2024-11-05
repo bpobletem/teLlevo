501042079e2f5773ff9d1878443cbc960c421000
Barbara Poblete <bpobletem96@gmail.com>
2024-11-05 10:00:53 -0300

    reestablecer pass

--- src/app/pages/auth/restablecer-password/restablecer-password.page.ts
+++ src/app/pages/auth/restablecer-password/restablecer-password.page.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { StorageService } from 'src/app/services/storage.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-restablecer-password',
@@ -10,39 +13,52 @@ import { Router } from '@angular/router';
 })
 export class RestablecerPasswordPage implements OnInit {
 
-  resetPasswordForm: FormGroup;
-  errorMessage: string = '';
-  successMessage: string = '';
+  correo = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router
-  ) {
-    this.resetPasswordForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]]
+  form: FormGroup;
+
+  firebaseSrv = inject(FirebaseService);
+  utilsSrv = inject(UtilsService);
+  localStorageSrv = inject(StorageService)
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private srv: StorageService,) {
+    this.form = this.formBuilder.group({
+      correo: ['', [Validators.required, Validators.email]],
     });
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    
+  }
 
-  onSubmit() {
-    const email = this.resetPasswordForm.get('email')?.value;
+  async submit() {
+    if (this.form.valid) {
+      const loading = await this.utilsSrv.loading();
+      await loading.present();
 
-    let usuariosRegistrados = JSON.parse(localStorage.getItem('users') || '[]');
-
-    const user = usuariosRegistrados.find((user: any) => user.email === email);
-
-    if (user) {
-      this.successMessage = 'Solicitud enviada. Revisa tu correo para restablecer tu contraseña.';
-      this.errorMessage = '';
-
-      setTimeout(() => {
-        this.router.navigate(['/nueva-password', { email: email }]);
-      }, 2000);
-
-    } else {
-      this.errorMessage = 'El correo ingresado no está registrado.';
-      this.successMessage = '';
+      this.firebaseSrv.sendRecoveryEmail(this.form.value.correo).then(res => {
+        this.utilsSrv.presentToast({
+          message: 'El correo de recuperación ha sido enviado',
+          duration: 1500,
+          color: 'primary',
+          position: 'bottom',
+          icon: 'alert-circle-outline'
+        })
+        this.router.navigate(['/home']);
+        this.form.reset();
+      }).catch(error => {
+        this.utilsSrv.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'bottom',
+          icon: 'alert-circle-outline'
+        })
+        console.log(error)
+      }).finally(() => {
+        loading.dismiss();
+      })
     }
   }
+
 }
