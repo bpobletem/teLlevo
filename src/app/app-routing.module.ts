ff8cd4341373a9629b3c3121cd1a8228bfe881b0
Barbara Poblete <bpobletem96@gmail.com>
2024-11-05 17:10:03 -0300

    fix guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { noAuthGuard } from './guards/no-auth.guard';
 import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
@@ -16,77 +15,62 @@ const routes: Routes = [
   },
   {
     path: 'buscar-viaje',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/buscar-viaje/buscar-viaje.module').then( m => m.BuscarViajePageModule)
   },
   {
     path: 'crear-viaje',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/crear-viaje/crear-viaje.module').then( m => m.CrearViajePageModule)
   },
   {
     path: 'crear-usuario',
-    canActivate: [noAuthGuard],
     loadChildren: () => import('./pages/auth/crear-usuario/crear-usuario.module').then( m => m.CrearUsuarioPageModule)
   },
   {
     path: 'iniciar-sesion',
-    canActivate: [noAuthGuard],
     loadChildren: () => import('./pages/auth/iniciar-sesion/iniciar-sesion.module').then( m => m.IniciarSesionPageModule)
   },
   {
     path: 'perfil',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   },
   {
     path: 'restablecer-password',
-    canActivate: [noAuthGuard],
     loadChildren: () => import('./pages/auth/restablecer-password/restablecer-password.module').then( m => m.RestablecerPasswordPageModule)
   },
   {
     path: 'nueva-password',
-    canActivate: [noAuthGuard],
     loadChildren: () => import('./pages/auth/nueva-password/nueva-password.module').then( m => m.NuevaPasswordPageModule)
   },
   {
     path: 'historial-viajes',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/historial-viajes/historial-viajes.module').then( m => m.HistorialViajesPageModule)
   },
   {
     path: 'registrar-auto',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/registrar-auto/registrar-auto.module').then( m => m.RegistrarAutoPageModule)
   },
   {
     path: 'listar-autos',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/listar-autos/listar-autos.module').then( m => m.ListarAutosPageModule)
   },
   {
     path: 'viaje-en-curso',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/viaje-en-curso/viaje-en-curso.module').then( m => m.ViajeEnCursoPageModule)
   },
   {
     path: 'solicitudes-de-viaje',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/solicitudes-de-viaje/solicitudes-de-viaje.module').then( m => m.SolicitudesDeViajePageModule)
   },
   {
     path: 'detalle-viaje',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/detalle-viaje/detalle-viaje.module').then( m => m.DetalleViajePageModule)
   },
   {
     path: 'editar-perfil',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/editar-perfil/editar-perfil.module').then( m => m.EditarPerfilPageModule)
   },
   {
     path: 'detalle-auto',
-    canActivate: [authGuard],
     loadChildren: () => import('./pages/detalle-auto/detalle-auto.module').then( m => m.DetalleAutoPageModule)
   },
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,25 +1,14 @@
-import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
-import { StorageService } from '../services/storage.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const firebaseSrv = inject(FirebaseService);
   const router = inject(Router);
-  const localStorageSrv = inject(StorageService)
-
-  const user = localStorageSrv.get(firebaseSrv.auth.currentUser.uid)
-  console.log(firebaseSrv.auth.currentUser)
-  console.log(user)
-
-  return new Promise((resolve) => {
-    firebaseSrv.auth.onAuthStateChanged((auth) => {
-      if (auth && user) {
-        resolve(true)
-      } else {
-        router.navigate(['/iniciar-sesion'])
-        resolve(false)
-      }
-    })
-  })  
+  if (firebaseSrv.auth.currentUser !== null) {
+    return true;
+  } else {
+    router.navigate(['/iniciar-sesion']);
+    return false; 
+  }
 };
