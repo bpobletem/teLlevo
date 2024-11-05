18ca41b0d4a599d796d20914001756569bd3cc84
Barbara Poblete <bpobletem96@gmail.com>
2024-11-05 10:00:45 -0300

    registro

--- src/app/pages/auth/crear-usuario/crear-usuario.page.ts
+++ src/app/pages/auth/crear-usuario/crear-usuario.page.ts
@@ -1,10 +1,12 @@
+import { setDoc } from '@angular/fire/firestore';
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { Usuario } from 'src/app/interfaces/interfaces';
-import { StorageService } from 'src/app/services/datos.service';
+import { Auto, Usuario } from 'src/app/interfaces/interfaces';
+import { StorageService} from 'src/app/services/storage.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-crear-usuario',
@@ -13,105 +15,103 @@ import { StorageService } from 'src/app/services/datos.service';
 })
 export class CrearUsuarioPage implements OnInit {
 
+  usuario: Usuario = {
+    uid: '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    correo: '',
+    esConductor: false
+  }
+
+  auto: Auto = {
+    marca: '',
+    modelo: '',
+    patente: '',
+    propietario: this.usuario
+  }
+
   registrarForm: FormGroup;
-  errorMessage: string = '';
-  successMessage: string = '';
+  autoForm: FormGroup;
+  firebaseSrv = inject(FirebaseService);
+  utilsSrv = inject(UtilsService);
+  localStorageSrv = inject(StorageService);
 
-usuario: Usuario = {
-  username: '',
-  password: '',
-  nombre: '',
-  apellido: '',
-  correo: ''
-}
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private alertController: AlertController,
-    private srv:StorageService
-  ) {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.registrarForm = this.formBuilder.group({
-      nombre: ['', [Validators.required]],
-      apellido: ['', [Validators.required]],
-      // rut: ['', [Validators.required]],
-      // esConductor: [false], 
-      // modeloAuto: [''],
-      // colorAuto: [''],
-      // patenteAuto: [''],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      uid: [''],
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      esConductor: [false],
     });
-
-    // this.registrarForm.get('esConductor')?.valueChanges.subscribe((esConductor) => {
-    //   this.toggleConductorFields(esConductor);
-    // });
+    this.autoForm = this.formBuilder.group({
+      modeloAuto: [''],
+      colorAuto: [''],
+      patenteAuto: ['']
+    });
   }
 
-  // toggleConductorFields(esConductor: boolean) {
-  //   if (esConductor) {
-  //     this.registrarForm.get('modeloAuto')?.setValidators([Validators.required]);
-  //     this.registrarForm.get('colorAuto')?.setValidators([Validators.required]);
-  //     this.registrarForm.get('patenteAuto')?.setValidators([Validators.required]);
-  //   } else {
-  //     this.registrarForm.get('modeloAuto')?.clearValidators();
-  //     this.registrarForm.get('colorAuto')?.clearValidators();
-  //     this.registrarForm.get('patenteAuto')?.clearValidators();
-  //   }
-
-  //   this.registrarForm.get('modeloAuto')?.updateValueAndValidity();
-  //   this.registrarForm.get('colorAuto')?.updateValueAndValidity();
-  //   this.registrarForm.get('patenteAuto')?.updateValueAndValidity();
-  // }
-
-  guardarUsuario() {
-    this.srv.set('Usuario', this.usuario);
-    console.log('Usuario guardado.');
+  ngOnInit(): void {
+    
   }
 
-   async onSubmit() {
-     if (this.registrarForm.valid) {
-      this.srv.set('Usuario', this.usuario);
+  async submit() {
+    if (this.registrarForm.valid) {
+      const loading = await this.utilsSrv.loading();
+      await loading.present();
+      console.log('empezando el submit')
+      await this.firebaseSrv.signUp(this.registrarForm.value as Usuario) 
+        .then(async res => {
+          console.log('esperando respuesta del signup')
+          await this.firebaseSrv.updateUser(this.registrarForm.value.nombre);
+          let uid = res.user.uid;
+          this.registrarForm.controls['uid'].setValue(uid);
+          this.setUser(uid);
+        }).catch(error => {
+          this.utilsSrv.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'primary',
+            position: 'bottom',
+            icon: 'alert-circle-outline'
+          });
+          console.log(error);
+        }).finally(() => {
+          loading.dismiss();
+        });
+    }
+  }
 
-      this.guardarUsuario();
-       // const nuevoUsuario = this.registrarForm.value;
+  async setUser(uid: string) {
+    if (this.registrarForm.valid) {
+      console.log('seteando usuario')
+      const loading = await this.utilsSrv.loading();
+      await loading.present();
 
-  //     let usuariosRegistrados = JSON.parse(localStorage.getItem('users') || '[]');
+      let path = `Usuario/${uid}`;
+      delete this.registrarForm.value.password;
 
-  //     const userExists = usuariosRegistrados.find((user: any) => user.email === nuevoUsuario.email);
+      this.firebaseSrv.setDocument(path, this.registrarForm.value).then(async res => {
+          this.localStorageSrv.set(uid, this.registrarForm.value);
+          this.router.navigate(['/iniciar-sesion']);
+          this.registrarForm.reset();
+        }).catch(error => {
+          this.utilsSrv.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'primary',
+            position: 'bottom',
+            icon: 'alert-circle-outline'
+          });
+          console.log(error);
+        }).finally(() => {
+          loading.dismiss();
+        });
+    }
+  }
+  
 
-  //     if (userExists) {
-  //       this.errorMessage = 'Ya existe una cuenta con este correo electrónico.';
-  //       this.successMessage = '';
-  //     } else {
-  //       usuariosRegistrados.push(nuevoUsuario);
-  //       localStorage.setItem('users', JSON.stringify(usuariosRegistrados));
 
-  //       localStorage.setItem('perfilUsuario', JSON.stringify(nuevoUsuario));
-
-  //       this.errorMessage = '';
-  //       this.successMessage = '';
-  //       await this.showSuccessAlert();
-  //     }
-  //   } else {
-  //     this.errorMessage = 'Por favor, rellene el formulario correctamente.';
-   }
- }
-
-  // async showSuccessAlert() {
-  //   const alert = await this.alertController.create({
-  //     header: 'Éxito',
-  //     message: 'Cuenta creada exitosamente.',
-  //     buttons: [{
-  //       text: 'OK',
-  //       handler: () => {
-  //         this.router.navigate(['/iniciar-sesion']);
-  //       }
-  //     }]
-  //   });
-
-  //   await alert.present();
-  // }
-
-  ngOnInit() {}
 }
