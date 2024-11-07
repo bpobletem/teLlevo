5e8893d1efeff3ac7eda67be508b156ed37e6db6
Barbara Poblete <bpobletem96@gmail.com>
2024-11-06 21:40:29 -0300

    auth guard fixeado

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,29 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
 import { inject } from '@angular/core';
+import { StorageService } from '../services/storage.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async (route, state) => {
   const firebaseSrv = inject(FirebaseService);
   const router = inject(Router);
-  if (firebaseSrv.auth.currentUser !== null) {
-    return true;
-  } else {
-    router.navigate(['/iniciar-sesion']);
-    return false; 
+  const storageSrv = inject(StorageService);
+
+  try {
+    const userUid = await storageSrv.get('sesion'); // Espera la promesa para obtener el userUid
+    console.log(userUid);
+    console.log(firebaseSrv.auth.currentUser);
+
+    if (firebaseSrv.auth.currentUser !== null) {
+      const uid = firebaseSrv.auth.currentUser.uid;
+      if (uid === userUid) {
+        return true;
+      }
+    }
+    await router.navigate(['/iniciar-sesion']);
+    return false;
+  } catch (error) {
+    console.error('Error obteniendo el UID del usuario:', error);
+    await router.navigate(['/iniciar-sesion']);
+    return false;
   }
 };
