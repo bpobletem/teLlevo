59739fe785d4e97ff6c43e43141bcca52c625c69
Barbara Poblete <bpobletem96@gmail.com>
2024-11-06 22:03:58 -0300

    logout en perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,6 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
+  firebaseSrv = inject(FirebaseService);
+  utilsSrv = inject(UtilsService);
 
   user = {
     nombre: '',
@@ -20,6 +24,17 @@ export class PerfilPage implements OnInit {
   };
 
   constructor(private router: Router) {}
+
+  signOut() {
+    this.firebaseSrv.signOut();
+    this.utilsSrv.presentToast({
+      message: "Sesión cerrada con éxito",
+      duration: 2500,
+      color: 'primary',
+      position: 'bottom',
+      icon: 'alert-circle-outline'
+    })
+  }
 
   ngOnInit(): void {
     
